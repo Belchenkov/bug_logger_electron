0c2d59cfa5e078bf2be58525e8efbcad845386bd
Aleksey Belchenkov <u608110@gmail.com>
2020-07-09 14:21:06 +0300

    Displaying Alerts

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
+import Alert from "react-bootstrap/Alert";
+
 import LogItem from "./LogItem";
 import AddLogItem from "./AddLogItem";
 
 const App = () => {
+	const [alert, setAlert] = useState({
+		show: false,
+		message: '',
+		variant: 'success'
+	});
 	const [logs, setLogs] = useState([
 		{
 			_id: 1,
@@ -29,15 +36,42 @@ const App = () => {
 		},
 	]);
 
+	const showAlert = (message, variant = 'success', seconds = 3000) => {
+		setAlert({
+			show: true,
+			message,
+			variant
+		});
+
+		setTimeout(() => {
+			setAlert({
+				show: false,
+				message: '',
+				variant: 'success'
+			});
+		}, seconds);
+	};
+
 	const addItem = item => {
+		if (item.text === '' || item.user === '' || item.priority === '') {
+			showAlert('Please enter all fields', 'danger');
+
+			return false;
+		}
+
 		item._id = Math.floor(Math.random() * 90000) + 10000;
 		item.created = new Date().toString();
 		setLogs([...logs, item]);
+
+		showAlert('Log Added');
 	};
 
 	return (
 		<Container>
 			<AddLogItem addItem={addItem} />
+
+			{ alert.show && <Alert variant={alert.variant}>{alert.message}</Alert> }
+
 			<Table>
 				<thead>
 					<tr>
